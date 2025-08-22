cb21de8662a3c1ad14640f6cf8f06e03058dae4e
Nik Nesladek <nik.nesladek@gmail.com>
2025-08-22 08:50:26 -0600

    feat: add app.js and app.css files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>This is your initial home page. Start building your app here!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
